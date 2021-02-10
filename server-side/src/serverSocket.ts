70b3f25c64a8c73ab26920bb350b53820cfe0d34
octavioduarte <octavioduarte2105@gmail.com>
2021-02-10 12:43:35 -0300

    feat: create method to save changes on DB

--- server-side/src/serverSocket.ts
+++ server-side/src/serverSocket.ts
@@ -1,6 +1,8 @@
 import { env } from './env'
 import { MongoHelper } from './db'
 import { Server, Socket } from "socket.io";
+import { ServiceTasks } from './service/tasks';
+import { ColumnTasks } from './types';
 
 
 const io: Server = new Server(Number(env.portSocketServer), env.optionsConnection);
@@ -17,9 +19,10 @@ MongoHelper.connect(env.dbConnection)
 
 io.on("connection", (clientSocket: Socket) => {
     console.log(`New socket: ${clientSocket.id}`)
-
-    clientSocket.on('update card position', updatedPositionCards => {
-        clientSocket.broadcast.emit('event updates cards', updatedPositionCards)
+    clientSocket.on('update card position', async (cardsNewPosition: ColumnTasks) => {
+        const serviceTasks = new ServiceTasks()
+        await serviceTasks.updateTasks(cardsNewPosition)
+        clientSocket.broadcast.emit('event updates cards', cardsNewPosition)
     })
 });
 
--- server-side/src/service/tasks.ts
+++ server-side/src/service/tasks.ts
@@ -1,10 +1,19 @@
 import { Collection, Cursor } from "mongodb";
 import { MongoHelper } from "../db";
+import { ColumnTasks } from "../types";
 
 export class ServiceTasks {
     async getTasks() {
         const tasksCollection: Collection = await MongoHelper.getCollection('tasks')
         const result = tasksCollection.find().toArray()
         return result
+    }
+
+    async updateTasks(tasks: ColumnTasks) {
+        const tasksCollection: Collection = await MongoHelper.getCollection('tasks')
+        tasks.forEach(async (columnWithTasks) => {
+            const { cards, droppableId } = columnWithTasks[0]
+            await tasksCollection.findOneAndUpdate({ droppableId }, { $set: { cards } })
+        })
     }
 }
