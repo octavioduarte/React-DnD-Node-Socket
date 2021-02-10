cddc8c49c6fae1fc85f1b23cc9f73d1d7f8f3b97
octavioduarte <octavioduarte2105@gmail.com>
2021-02-10 02:21:22 -0300

    refactor: remove mock data

--- server-side/src/service/tasks.ts
+++ server-side/src/service/tasks.ts
@@ -3,8 +3,8 @@ import { MongoHelper } from "../db";
 
 export class ServiceTasks {
     async getTasks() {
-        const tasksCollection: Collection | void = await MongoHelper.getCollection('tasks')
-        const result =  tasksCollection.find().toArray()
+        const tasksCollection: Collection = await MongoHelper.getCollection('tasks')
+        const result = tasksCollection.find().toArray()
         return result
     }
 }
