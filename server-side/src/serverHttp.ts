0b8c83e1d1339272db614100d111b0bcfc3a4307
octavioduarte <octavioduarte2105@gmail.com>
2021-02-09 23:33:46 -0300

    feat: create connection method between http server and mongodb

--- server-side/src/serverHttp.ts
+++ server-side/src/serverHttp.ts
@@ -1,8 +1,16 @@
 import serverHttp from './config/appServerHttp'
+import { MongoHelper } from './db';
 import { env } from './env';
 
+MongoHelper.connect(env.dbConnection)
+    .then(() => {
+        console.info('[SERVER HTTP] Connection to database successful')
+        serverHttp.listen(env.portHttpServer, () => {
+            console.info(`Server http running at port: ${env.portHttpServer}`)
+        })
+    })
+    .catch((error: Error) => {
+        console.error(`[SERVER HTTP] Error establishing a database connection: ${error.message}`)
+    })
 
 
-serverHttp.listen(env.portHttpServer, () => {
-    console.info(`Server http running at port: ${env.portHttpServer}`)
-})
